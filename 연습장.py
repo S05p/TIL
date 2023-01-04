6a851276af6550059596be1390ba202ee89ed214
S05p <rlarkdtjq590@naver.com>
2023-01-04 13:35:40 +0900

    일단 til 갱신

---
+++ 연습장.py
@@ -0,0 +1,32 @@
+# chess = [1, 1, 2, 2, 2, 8]
+
+# n = list(map(int,input().split()))
+
+# for i in range(6):
+#     print(f'{chess[i]-n[i]}', end=" ")
+print('----------------')
+# a = 385
+
+# print(type(a))
+
+# a_str = str(a)
+
+# print(a[2])
+
+# list(a_str)
+print('----------------')
+
+n= int(input(''))
+
+a=0
+list_a =[]
+
+while a<n:
+    a = a+1
+    list_a.append(a)
+
+list_a_sum = sum(list_a)
+
+print(list_a_sum)
+
+    
