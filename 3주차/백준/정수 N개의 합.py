4dce071474930de34949b5db4a6ea870c306b5ee
S05p <rlarkdtjq590@naver.com>
2023-01-13 17:31:57 +0900

    잔디 심기

---
+++ 3주차/백준/정수 N개의 합.py
@@ -0,0 +1,7 @@
+def solve(a):
+    
+    return sum(a)
+
+a = [1,2,3,4,5]
+
+print(solve(a))
--- 3주차/백준/평균은 넘겠지.py
+++ 3주차/백준/평균은 넘겠지.py
@@ -3,5 +3,9 @@ C = int(input())
 for i in range(C):
     test_list = list(map(int,input().split()))
     test_aver = (sum(test_list[1:])/test_list[0])
+    j_list=[]
     for j in test_list[1:]:
-        
+        if j > test_aver:
+            j_list.append(j)
+            
+    print('{:.3f}'.format((len(j_list)/(len(test_list)-1))*100,6),'%',sep='')
