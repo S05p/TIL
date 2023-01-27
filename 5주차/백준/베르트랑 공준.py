f5f531501400fdfe5cd11d8e34f2a804cfcfba44
S05p <rlarkdtjq590@naver.com>
2023-01-27 17:26:41 +0900

    금요일

--- 5주차/백준/베르트랑 공준.py
+++ 5주차/백준/베르트랑 공준.py
@@ -9,15 +9,19 @@ def prime_number(x):
             if x%i == 0:
                 return False
         return True
-prime_number_list=[]
 while True:
     n = int(sys.stdin.readline())
     cnt = 0
     if n == 0:
         break
+    elif n == 1:
+        print(1)
+        continue
     for j in range(n+1,2*n):
         if prime_number(j) == True:
             cnt += 1
-            prime_number_list.append(j)
             
     print(cnt)
+    
+## 아이디어는 모든 소수를 다 카운트해가면서 할 필요가 없다는 것
+# 그래서 리스트에 추가를 해서 그 뒤로 모두 비교하면 되지 않을까? 하는 거시다
