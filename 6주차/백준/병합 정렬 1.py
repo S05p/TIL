4fc367c322caf3b23184762aa290d4952f4f5b01
S05p <rlarkdtjq590@naver.com>
2023-02-05 23:54:49 +0900

    일요일 공부

--- 6주차/백준/병합 정렬 1.py
+++ 6주차/백준/병합 정렬 1.py
@@ -1,9 +1,40 @@
+
+
 N, K = map(int,input().split())
 N_list = list(map(int,input().split()))
+N_list_s = sorted(N_list)
+cnt = 0
+cnt_n = 0
+N_i = (N//2)            
 
-while True:
-    N_1 = []
-    if N_list != N_list.sort():
-        for i in range(len(N_list)-1):
-            N_1.append(N_list[i+1]-N_list[i])
-            
+N_f = sorted(N_list[:(N_i)+1])
+N_b = sorted(N_list[(N_i)+1:])
+
+N_dict = {}
+
+for i in range(N_i):
+    cnt += 1
+    cnt_n = N_list[i]
+    N_dict[cnt] = N_list[i]
+    
+for j in range(len(N_f)):
+    cnt += 1
+    cnt_n = N_f[j]
+    N_dict[cnt] = N_f[j]
+    
+for k in range(len(N_b)):
+    cnt += 1
+    cnt_n = N_b[k]
+    N_dict[cnt] = N_b[k]
+    
+for q in range(N):
+    cnt += 1
+    cnt_n = N_list_s[q]
+    N_dict[cnt] = N_list_s[q]
+
+
+
+if K > cnt:
+    print(-1)
+else:
+    print(N_dict[K])
--- 6주차/백준/연습장.py
+++ 6주차/백준/연습장.py
@@ -1,2 +1,7 @@
-for i in range(10,0,-1):
-    print(i)
+N_list = []
+
+N_f = [1, 4, 5]
+N_b = [2,3]
+
+N_list = N_f + N_b
+print(N_list)
